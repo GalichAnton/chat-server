765ee2e8dac96339f58442b2000b8c249653668d
aagalich <aagalich@avito.ru>
2024-02-10 10:26:35 +0300

    add chat api

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,64 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	desc "github.com/GalichAnton/chat-server/pkg/chat_v1"
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedChatV1Server
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Chat created: %v", req.GetUsernames())
+	log.Printf("Context =%v", ctx)
+
+	return &desc.CreateResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
+func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("Message = %s received", req.GetText())
+	log.Printf("Context =%v", ctx)
+
+	empty := emptypb.Empty{}
+
+	return &empty, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Chat with [id]=%d deleted", req.GetId())
+	log.Printf("Context =%v", ctx)
+
+	empty := emptypb.Empty{}
+
+	return &empty, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
